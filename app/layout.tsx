b74901ed6fe308acaa4caf5e1b4690830b116048
mkmosaikpartners <mk@mosaik.partners>
2026-01-20 18:11:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: "ECHO Brand Self-Assessment",
-  description: "ECHO Brand Self-Assessment Webapp",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="de">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
+export default function ResultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
 }
